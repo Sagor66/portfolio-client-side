256b32af680539fd5220e2b3f8711d4c0f00118f
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-06-21 17:23:29 +0600

    added navbar

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../pages/Shared/NavBar";
+import Footer from "../pages/Shared/Footer";
+
+const Main = () => {
+  return (
+    <div className="font-nunito">
+      <div className="min-h-screen">
+        <NavBar></NavBar>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
